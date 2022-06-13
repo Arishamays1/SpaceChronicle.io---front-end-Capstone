b0e9c2ffa63936131fd40e086b84926e1388a318
arishamays1 <1arishamays1@gmail.com>
2022-06-13 05:56:37 -0400

    css and paragraph added for secret page

--- frontend/src/Pages/Dedicate.js
+++ frontend/src/Pages/Dedicate.js
@@ -16,10 +16,16 @@ const Dedicate= ()=>{
             This website is dedicated to
         </h1>
            <h2>
-            Larry Mays
+           my dad, Larry Mays
             </h2>
            <Carousel/>
-        
+        <h1>
+            The best dad in the world.
+        </h1>
+        <p className='prgh'>My dad's love for computers, astronomy and the sciences, and art was passed down to me at a very early age.
+            He spearheaded my interest in computer science at the age of 3 and here I am, officially a software engineer, 20 years later.
+            So I dedicate my page to you dad. Thank you.
+        </p>
         
         </div>
   )
